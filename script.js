a7cac3c4d9594833338b4fcebba07a4d319afe5f
Kylie <ka-scott@hotmail.co.uk>
2021-04-07 16:53:35 +0100

    All functionality done

--- script.js
+++ script.js
@@ -80,21 +80,3 @@ function calcHourly(wage, hours) {
 }
 
 // ---------- jobinfo.html ----------
-
-let base_url = "http://api.lmiforall.org.uk/api/v1/soc/search/ ";
-
-const jobinfo_form = document.getElementById("jobinfo-form");
-jobinfo_form.addEventListener("submit", function (e) {
-  console.log("test");
-  e.preventDefault();
-  //   let jobtitle = document.getElementById("jobtitle").value;
-  //   console.log(jobtitle.value);
-  //   // to do - sanitise input
-  //   let url_query = base_url + jobtitle;
-
-  //   fetch(url_query)
-  //     .then((response) => response.json())
-  //     .then((json_response) => {
-  //       console.log(json_response.result);
-  //     });
-});
